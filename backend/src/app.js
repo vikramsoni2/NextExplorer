246d461c2429e47b2b170660157ff7d883e1db1a
Vikram <vikram9880@gmail.com>
2026-01-30 21:11:49 +0100

    remove limit when opening file

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,8 +27,8 @@ const initializeApp = async () => {
    configureHttpLogging(app);
 
    configureCors(app);
-   app.use(express.json({ limit: '50mb' }));
-   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+   app.use(express.json());
+   app.use(express.urlencoded({ extended: true }));
    app.use(cookieParser());
    logger.debug('Mounted cookie parser middleware');
 
